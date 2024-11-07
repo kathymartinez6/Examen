85ccebb10a08e9fcdbbc7e9733e4457c1bb4e908
kathy_chell <chela.lara76@gmail.com>
2024-11-06 18:41:55 -0600

    crear y rellenar interfaz

---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen
+{
+    public interface IAsignatura
+    {
+        double CalcularNotaFinal();
+        double CalcularNotaFinal(double n1, double n2, double n3);
+        string MensajeNotaFinal(double notaFinal);
+        void Imprimir();
+    }
+}
